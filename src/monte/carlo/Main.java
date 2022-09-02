fc88d06d661c65efdadf92747a61e36e384b1253
ppragerurbina788 <ppragerurbina788@student.fuhsd.org>
2022-09-02 14:39:57 -0700

    Added gamblers ruin

--- src/monte/carlo/Main.java
+++ src/monte/carlo/Main.java
@@ -3,10 +3,16 @@ package monte.carlo;
 public class Main {
 
     public static void main(String[] args) {
+
+        GamblersRuin ruin =  new GamblersRuin(1000);
+        System.out.println(ruin.runSuccessSimulation());
+        System.out.println(ruin.runBetSimulation());
+
+        /*
 	ParticleDiffusion a =  new ParticleDiffusion(1.0, 1000);
     a.fillTable(100);
     a.getTable().outputCoords();
-a.getTable().outputPlot((Integer x)-> x,(Double y) -> y, 2 );
-
+    a.getTable().outputPlot((Integer x)-> x,(Double y) -> y, 2 );
+*/
     }
 }
